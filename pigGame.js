e45f55bdfc2df4429d601ffebba3a89c99409d84
Shaktish <shakiddo@gmail.com>
2017-10-27 08:03:02 +0530

    Update pigGame.js

--- pigGame.js
+++ pigGame.js
@@ -37,22 +37,50 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
 		roundScore += dice;
 		document.getElementById('current-' + activePlayer).textContent = roundScore;
 	} else {
-		activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-		roundScore = 0;
-
-		document.getElementById('current-0').textContent = 0;
-		document.getElementById('current-1').textContent = 0;
-
-		document.querySelector('.player-0-panel').classList.toggle('active');
-		document.querySelector('.player-1-panel').classList.toggle('active');
-
-		//document.querySelector('.player-0-panel').classList.remove('active');
-		//document.querySelector('.player-1-panel').classList.add('active');
-		document.querySelector('.dice').style.display = 'none';
+		//nextplayer
+		nextPlayer();
 	}
-
 	
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function () {
+	// Add current score to global score
+		scores[activePlayer] += roundScore;
+
+	// update the UI 
+		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+	// check if player won the game
+		if(scores[activePlayer] >= 20) {
+			document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+			document.querySelector('.dice').style.display = 'none';
+			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+			document.querySelector('.player-' + activePlayer + '-panel').classList.add('active');
+		} else {
+	// next player
+			nextPlayer();
+		}
+
+r
+
+
+
+});
+
+function nextPlayer() {
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	roundScore = 0;
+
+	document.getElementById('current-0').textContent = 0;
+	document.getElementById('current-1').textContent = 0;
+
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
+
+	//document.querySelector('.player-0-panel').classList.remove('active');
+	//document.querySelector('.player-1-panel').classList.add('active');
+	document.querySelector('.dice').style.display = 'none';
+}
 
 
 
